afa58d7ea342ef11ee60d60bace42ebcc7939474
Ben Swift <student@GHSStudentSwiftBen-2.local>
2015-01-05 11:48:01 -0500

    upload first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+for (var i = 0; i < 101; i += 5) {
+  console.log(i);
+  return "Hello";
+}
+
+var hello = "Hello";
+
+if (hello.length < 10) {
+  console.log("Hello!");
+} else {
+  console.log("Goodbye!");
+}
